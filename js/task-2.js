fd1c96d74460c650c823a2b02d65de1d78b4c73f
Dmitry Nezvinskyi <Nezvinskyi@users.noreply.github.com>
2020-12-02 08:56:37 +0100

    Adds task 2

---
+++ js/task-2.js
@@ -0,0 +1,17 @@
+const calculateEngravingPrice = function(message, pricePerWord) {
+  let engravingLength = 0;
+  let price = 0;
+
+  engravingLength = message.split(' ').length;
+  price = engravingLength * pricePerWord;
+
+  return price;
+};
+
+console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10));
+
+console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 20));
+
+console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40));
+
+console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20));
